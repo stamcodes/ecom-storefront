73ed83b925ffc870cf4d67ab063237ea4f624ebf
Syed Taneer <stamegy@gmail.com>
2026-07-28 13:07:43 +0500

    Main.py fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -59,7 +59,7 @@ ROUTERS = [
 ]
 
 for router, tag in ROUTERS:
-    app.include_router(router, prefix="/api/v1", tags=[tag])
+    app.include_router(router, tags=[tag])
 
 
 @app.on_event("startup")
